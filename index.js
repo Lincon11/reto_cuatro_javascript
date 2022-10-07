2fe58dabea47bad65aa34d608107c299e3c507bd
Lincon11 <eibraham37@gmail.com>
2022-10-07 14:56:25 -0500

    Arreglando condiciones

--- index.js
+++ index.js
@@ -7,20 +7,21 @@ console.log('\n');
 
 let puntaje = scanf('%d');
 
-if (0 <= puntaje <= 3) {
+if (puntaje == 0  || puntaje < 3) {
     console.log("Tu calificacion es: muy deficiente")
-} else if (3 <= puntaje <= 5) {
+
+} else if (puntaje < 3 || puntaje < 5) {
     console.log("Tu calificacion es: deficiente")
 
-} else if (5 <= puntaje <= 6){
+} else if (puntaje < 5 || puntaje < 6){
     console.log("Tu calificacion es: insuficiente")
     
-} else if (6 <= puntaje <= 7){
+} else if (puntaje < 6 || puntaje < 7){
     console.log("Tu calificacion es: bien")
 
-} else if (7<= puntaje <= 9){
+} else if (puntaje < 7 || puntaje <= 9){
     console.log("Tu calificacion es: notable")
 
-} else if (9 <= puntaje <= 10){
+} else if ( puntaje == 10){
     console.log("Tu calificacion es: sobresaliente")
 }
